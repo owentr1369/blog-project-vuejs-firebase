8e2cd309f985ca54f6066affb886319ae9c2d4c3
owentr1369 <tamvaa1306@gmail.com>
2022-05-17 22:33:24 +0700

    add BlogPostComp template

---
+++ src/components/BlogPostComp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+        <p class="content-preview" v-else>{{ post.blogHTML }}</p>
+        <!-- Oke -->
+        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+          Login/Register
+          <!-- <ArrowIcon class="arrow arrow-light" /> -->
+        </router-link>
+        <router-link class="link link-light" v-else to="#">
+          View The Post
+          <!-- <ArrowIcon class="arrow arrow-light" /> -->
+        </router-link>
+      </div>
+      <div class="blog-photo">
+        <img
+          :src="require(`../assets/blogPhotos/${post.photo}.jpg`)"
+          v-if="post.welcomeScreen"
+          alt=""
+        />
+        <img
+          :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)"
+          v-else
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import ArrowIcon from "../assets/Icons/arrow-right-light.svg";
+export default {
+  name: "blog-post",
+  props: ["post"],
+  // components: { ArrowIcon },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPostComp :post="welcomeScreen" />
+    <BlogPostComp
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
   </div>
 </template>
 
 <script>
+import BlogPostComp from "../components/BlogPostComp.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPostComp },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "WELCOME!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
