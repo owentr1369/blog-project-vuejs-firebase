e6c8de510ffdfbbdfb26529d6ab21d07633c1b83
owentr1369 <tamvaa1306@gmail.com>
2022-05-18 14:52:49 +0700

    styling input checkbox

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -28,4 +28,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.blog-cards {
+  position: relative;
+
+  .toggle-edit {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: -70px;
+    right: 0;
+
+    span {
+      margin-right: 16px;
+    }
+    input[type="checkbox"] {
+      position: relative;
+      border: none;
+      -webkit-appearance: none;
+      background: #fff;
+      outline: none;
+      width: 80px;
+      height: 30px;
+      border-radius: 20px;
+      box-shadow: 0 4px 6px -1px rgba($color: #000000, $alpha: 0.1),
+        0 2px 4px -1px rgba($color: #000000, $alpha: 0.06);
+      &::before {
+        content: "";
+        position: absolute;
+        width: 30px;
+        height: 30px;
+        border-radius: 20px;
+        top: 0;
+        left: 0;
+        background: #303030;
+        transform: scale(1.1);
+        transition: 750ms ease all;
+        box-shadow: 0 4px 6px -1px rgba($color: #000000, $alpha: 0.1),
+          0 2px 4px -1px rgba($color: #000000, $alpha: 0.06);
+      }
+    }
+    input:checked[type="checkbox"]::before {
+      background: #fff;
+      left: 52px;
+    }
+  }
+}
 </style>
