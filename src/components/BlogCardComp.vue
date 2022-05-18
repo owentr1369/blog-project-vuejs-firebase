4114b3273e63a8b09a5d54a465f7fa09f26b1f8e
owentr1369 <tamvaa1306@gmail.com>
2022-05-18 09:41:08 +0700

    Add blogcards template

---
+++ src/components/BlogCardComp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="blog-card">
+    <div class="icons">
+      <div class="icon">
+        <Edit class="icon" />
+      </div>
+      <div class="icon">
+        <Delete class="icon" />
+      </div>
+    </div>
+    <img
+      :src="require(`../assets/blogCards/${post.blogCoverPhoto}.jpg`)"
+      alt=""
+    />
+    <div class="info">
+      <h4>{{ post.blogTitle }}</h4>
+      <h6>Posted on: {{ post.blogDate }}</h6>
+      <router-link class="link" to="#">
+        View the post <Arrow class="arrow" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Arrow from "../assets/Icons/arrow-right-light.svg";
+import Edit from "../assets/Icons/edit-regular.svg";
+import Delete from "../assets/Icons/trash-regular.svg";
+
+export default {
+  name: "blogCard",
+  props: ["post"],
+  components: { Arrow, Edit, Delete },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,23 @@
       v-for="(post, index) in sampleBlogPost"
       :key="index"
     />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCardComp v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPostComp from "../components/BlogPostComp.vue";
+import BlogCardComp from "../components/BlogCardComp.vue";
 export default {
   name: "Home",
-  components: { BlogPostComp },
+  components: { BlogPostComp, BlogCardComp },
   data() {
     return {
       welcomeScreen: {
@@ -35,7 +44,39 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "May 1, 2021",
+        },
+      ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
