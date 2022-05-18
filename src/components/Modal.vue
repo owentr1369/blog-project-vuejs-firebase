7bdb48dd9e8ddcbd330b49f5df8c49ec8348865f
owentr1369 <tamvaa1306@gmail.com>
2022-05-18 21:47:22 +0700

    styling modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -8,8 +8,41 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["modalMessage"],
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 101;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba($color: #000000, $alpha: 0.7);
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 8px;
+    width: 300px;
+    padding: 40px 30px;
+    background-color: #fff;
+    p {
+      text-align: center;
+    }
+    button {
+      align-self: center;
+    }
+  }
+}
 </style>
