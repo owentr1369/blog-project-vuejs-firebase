6fc9b0c7d799acc9f9808b10a9bf55a68e6020e5
owentr1369 <tamvaa1306@gmail.com>
2022-05-18 21:39:44 +0700

    create modal teamplate

---
+++ src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <p>{{ this.modalMessage }}</p>
+      <button @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
