4eed68756f5d561a049e78ef9ee18ab0cd362c6f
owentr1369 <tamvaa1306@gmail.com>
2022-05-18 12:36:39 +0700

    add blogs view template

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Blogs</h1>
+  <div class="blog-card-wrap">
+    <div class="blog-cards container">
+      <div class="toggle-edit">
+        <span>Toggle Editing Post</span>
+        <input type="checkbox" v-model="editPost" />
+      </div>
+      <BlogCardComp
+        :post="post"
+        v-for="(post, index) in sampleBlogCards"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import BlogCardComp from "../components/BlogCardComp.vue";
 export default {
   name: "blogs",
+  components: { BlogCardComp },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards;
+    },
+  },
 };
 </script>
 
